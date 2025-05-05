29056e0199506f2bc6592d53fb6fe8ea17becd80
Bayfr <bayufrdinand@gmail.com>
2025-05-06 02:55:42 +0700

    Fillament configuration and db migration

---
+++ app/Models/News.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class News extends Model
+{
+    protected $fillable = [
+        'author_id',
+        'news_category_id',
+        'title',
+        'slug',
+        'thumbnail',
+        'content'
+    ];
+
+     
+    public function Author()
+    {
+        return $this->belongsTo(author::class);
+    }
+ 
+    public function newsCategory()
+    {
+        return $this->belongsTo(newsCategory::class);
+    }
+    
+    public function Banner()
+    {
+        return $this->hasOne(Banner::class);
+    }
+
+}
